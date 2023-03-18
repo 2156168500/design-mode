d5afb4155d1640163d296d58abbe388f0927d11c
梦中梦 <9451454+nzghfjh@user.noreply.gitee.com>
2023-03-18 16:53:20 +0800

    简单工厂模式+工厂方法模式实现去除代码中很多的if——else(细节调整)

--- src/stategy/example4/ReceiptStrategyContext.java
+++ src/stategy/example4/ReceiptStrategyContext.java
@@ -1,15 +1,16 @@
 package stategy.example4;
 
+import org.jetbrains.annotations.NotNull;
 import stategy.example2.Receipt;
 import stategy.example3.ReceiptStrategy;
 
 public class ReceiptStrategyContext {
-    public void handlerReceipt(Receipt receipt){
-          //创建策略
-            ReceiptStrategyFactory.init();
-          //获取 策略
-            ReceiptStrategy receiptStrategy = ReceiptStrategyFactory.getReceiptStrategy(receipt.getType());
-           //使用策略
-           receiptStrategy.handlerReceipt(receipt);
+    public void handlerReceipt(@NotNull Receipt receipt){
+        //创建策略
+        ReceiptStrategyFactory.init();
+        //获取 策略
+        ReceiptStrategy receiptStrategy = ReceiptStrategyFactory.getReceiptStrategy(receipt.getType());
+       //使用策略
+       receiptStrategy.handlerReceipt(receipt);
     }
 }
